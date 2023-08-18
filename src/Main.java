a51fd9a8d715a7858bc24538843769b51bb4f43d
Ekaterina <shi.e.m@yandex.ru>
2023-08-18 23:22:57 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,15 @@ public class Main {
         int price = 10_000;
         int miles = service.calculate(price); // должно получиться 500
         System.out.println(miles);
+        int price1 = 19;
+        int miles1 = service.calculate(price1);
+        System.out.println(miles1);
+        int price2 = 20;
+        int miles2 = service.calculate(price2);
+        System.out.println(miles2);
+        int price3 = 21;
+        int miles3 = service.calculate(price3);
+        System.out.println(miles3);
     }
 
 }
